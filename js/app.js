419c61375d2ae04678dbda634dd2e06fbca9d580
kevinreber <kevinreber1@gmail.com>
2019-11-18 21:03:38 -0800

    CSS 95% done, need to find out how to fit charts. Just begun JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const alert = document.getElementById('alert-bar');
+
+alert.innerHTML = `
+<div class="alert-banner">
+    <p><strong>ALERT:</strong> You have <strong>5</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+</div>`;
+
+alert.addEventListener('click', (e) =>{
+    if (e.target.classList.contains('alert-banner-close')){
+        alert.style.display = 'none';
+    }
+});
--- js/charts.js
+++ js/charts.js
@@ -1,31 +1,38 @@
 //CHART COLORS
-const primary = 'rgb(116,119,191,.3)';
-const secondary = 'rgb(77,76,114,.8)';
-const tertiary = 'rgb(116,119,191,.8)';
+const colorPrimary = 'rgb(116,119,191,.3)';
+const colorSecondary = 'rgb(77,76,114,.8)';
+const colorTertiary = 'rgb(116,119,191,.8)';
 
-const primaryHover = 'rgb(116,119,191)';
-const secondaryHover = 'rgb(77,76,114)';
-const tertiaryHover = 'rgb(116,119,191)';
+const colorPrimaryHover = 'rgb(116,119,191)';
+const colorSecondaryHover = 'rgb(77,76,114)';
+const colorTertiaryHover = 'rgb(116,119,191)';
+
+//SELECTORS
+const alertBar = document.getElementById('alert-bar');
 
 //AREA CHART
 var ctx1 = document.getElementById('areaChart').getContext('2d');
 var areaChart = new Chart(ctx1, {
     // The type of chart we want to create
     type: 'line',
-
     // The data for our dataset
     data: {
+        //labels are linked to datasets.data, if no labels are defined datasets.data will not display
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: primary,
-            borderColor: secondary,
+            //Remove default legend
+            label: '',
+            backgroundColor: colorPrimary,
+            borderColor: colorSecondary,
+            borderWidth: 1,
+            pointHoverBackgroundColor: colorPrimaryHover,
+            pointHoverRadius: 5,
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
-    },
+    }
 
     // Configuration options go here
-    options: {}
+    // options: {}
 });
 
 //BAR CHART
@@ -33,12 +40,21 @@ let ctx2 = document.getElementById('barChart').getContext('2d');
 let barChart = new Chart(ctx2, {
     type: 'bar',
     data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
         datasets: [{
+            label: '',
+            backgroundColor: colorPrimary,
+            borderColor: colorSecondary,
+            borderWidth: 1,
+            hoverBackgroundColor: colorPrimaryHover,
+            hoverBorderWidth: 3,
             barPercentage: 0.5,
             barThickness: 6,
             maxBarThickness: 8,
             minBarLength: 2,
-            data: [10, 20, 30, 40, 50, 60, 70]
+            //Bars are measured in units of 10
+            //Lowest number automatically updates to reflect changes in data 
+            data: [15, 20, 30, 40, 50, 60, 70]
         }]
     }
 });
@@ -54,7 +70,7 @@ let doughnutChart = new Chart(ctx3, {
             'Desktops'
         ],
         datasets: [{
-            backgroundColor: [tertiary, secondary, primary],
+            backgroundColor: [colorTertiary, colorSecondary, colorPrimary],
             // hoverBackgroundColor: [tertiaryHover],
             data: [65, 10, 25]
         }]
@@ -67,3 +83,22 @@ let doughnutChart = new Chart(ctx3, {
 
 
 });
+
+// //Add data
+// function changeData(chart, newLabels) {
+//     console.log('works');
+//     console.log(chart.data.labels.value);
+    
+//     chart.data.labels = [];
+//     newLabels.forEach(function (label) {
+//         chart.data.labels.push(label);
+//     })
+//     chart.update();
+// }
+
+//data.labels
+//data.datasets.data
+
+// alertBar.addEventListener('click', ()=>{
+//     changeData(ctx1, ['yes','no']);
+// });
